de6068be2a56323518bccd456b534dfdc80a9b71
YojimboAndGallery <yojimbosushi@gmail.com>
2025-06-28 05:43:11 -0700

    feat: add express static server

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+
+// 정적 파일(HTML · CSS · JS) 서빙
+app.use(express.static(path.join(__dirname)));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(Server running on ));
